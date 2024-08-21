0d2ef04c22ecbc1601f7ae3999fee382a29cbbd4
Erico Silva <augusto.ericosilva@gmail.com>
2024-08-21 19:26:23 -0300

    Add elapsed time to scenario run

--- src/runner.py
+++ src/runner.py
@@ -4,6 +4,7 @@ from qiskit import QuantumCircuit
 from qiskit.result.result import Result
 
 from scenario import Scenario
+from utils import print_exec_time
 
 class RunnerResult(TypedDict):
     result: Result
@@ -14,6 +15,7 @@ class Runner:
     def __init__(self, transpiled_circuit: QuantumCircuit):
         self._transpiled_circuit = transpiled_circuit
     
+    @print_exec_time
     def _run_one(self, scenario: Scenario):
         # [TODO] raise exception if it can't run a scenario for the given backend
         #   i.e. the ideal backend is too different from the real backend
